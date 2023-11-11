1fac6f9a391f18e15f308e56a8d966e09c707fcd
kvandavasi <100835776+kvandavasi@users.noreply.github.com>
2023-11-11 12:09:11 +0530

    Update KafkaProducerService.java

--- src/main/java/com/example/herokudemo/KafkaProducerService.java
+++ src/main/java/com/example/herokudemo/KafkaProducerService.java
@@ -1,6 +1,8 @@
+package com.example.herokudemo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+
 
 @Service
 public class KafkaProducerService {
