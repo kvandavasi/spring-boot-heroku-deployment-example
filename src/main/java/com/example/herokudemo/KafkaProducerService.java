479662531793e917d26e4680b08a62e03932da76
kvandavasi <100835776+kvandavasi@users.noreply.github.com>
2023-11-11 11:29:41 +0530

    Create KafkaProducerService.java

---
+++ src/main/java/com/example/herokudemo/KafkaProducerService.java
@@ -0,0 +1,17 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaProducerService {
+
+    private static final String TOPIC = "my-topic";
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    public void produceMessage(String message) {
+        kafkaTemplate.send(TOPIC, message);
+        System.out.println("Message sent: " + message);
+    }
+}
