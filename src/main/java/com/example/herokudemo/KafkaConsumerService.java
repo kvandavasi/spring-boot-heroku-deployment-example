b861ca5949a302c1cfd8202f99f4b69f980ed174
kvandavasi <100835776+kvandavasi@users.noreply.github.com>
2023-11-11 11:30:15 +0530

    Create KafkaConsumerService.java

---
+++ src/main/java/com/example/herokudemo/KafkaConsumerService.java
@@ -0,0 +1,13 @@
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaConsumerService {
+
+    private static final String TOPIC = "my-topic";
+
+    @KafkaListener(topics = TOPIC, groupId = "my-group")
+    public void consumeMessage(String message) {
+        System.out.println("Message received: " + message);
+    }
+}
