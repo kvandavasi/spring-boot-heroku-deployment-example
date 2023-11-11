40a8d64e53083e618c718ed5a89bce7af1122782
kvandavasi <100835776+kvandavasi@users.noreply.github.com>
2023-11-11 11:32:11 +0530

    Update IndexController.java

--- src/main/java/com/example/herokudemo/IndexController.java
+++ src/main/java/com/example/herokudemo/IndexController.java
@@ -2,12 +2,23 @@ package com.example.herokudemo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class IndexController {
+    @Autowired
+    private KafkaProducerService producerService;
 
     @GetMapping("/")
     public String index() {
         return "Hello there! I'm running.";
+    }
+    @GetMapping("/produce")
+    public String produceMessage(@RequestParam String message) {
+        producerService.produceMessage(message);
+        return "Message sent to Kafka!";
     }
 }
