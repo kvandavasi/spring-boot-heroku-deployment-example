3b12ec44ce7e800da769c01cf887d046c0f516ca
kvandavasi <100835776+kvandavasi@users.noreply.github.com>
2023-11-11 11:52:11 +0530

    Update IndexController.java

--- src/main/java/com/example/herokudemo/IndexController.java
+++ src/main/java/com/example/herokudemo/IndexController.java
@@ -6,6 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class IndexController {
